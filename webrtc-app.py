67f96b726f0581b99ce4fcd39b52fa3799ac0cde
mohapard <mohapard@gmail.com>
2022-11-08 10:35:07 +0200

    Update webrtc-app.py

--- webrtc-app.py
+++ webrtc-app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
 import av
 import threading
-from utils.utils import mediapipepose
+from utils import mediapipepose
 
 mpipe = mediapipepose()
 RTC_CONFIGURATION = RTCConfiguration(
