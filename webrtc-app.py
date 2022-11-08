e1cf1a4a23e890c239d3c7e402d0351003c9e2a3
mohapard <mohapard@gmail.com>
2022-11-08 11:29:20 +0200

    Update webrtc-app.py

--- webrtc-app.py
+++ webrtc-app.py
@@ -4,7 +4,8 @@ import av
 import threading
 from utils import mediapipepose
 import time
-@st.cache(allow_output_mutation=True)
+import gc
+
 mpipe = mediapipepose()
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -33,9 +34,10 @@ if task_name == task_list[0]:
             if self.style != new_style:
                 with self.model_lock:
                     self.style = new_style
+              
        
         def recv(self, frame):
-            
+            gc.collect()
             # img = frame.to_ndarray(format="bgr24")
             img = frame.to_image()
             if self.style == style_list[1]:
@@ -45,7 +47,7 @@ if task_name == task_list[0]:
                 try:
                     time.sleep(2)
                     img = frame.to_ndarray(format="bgr24")
-                    @st.cache(allow_output_mutation=True)
+                    
                     img = mpipe.processImage(img)
                     return av.VideoFrame.from_ndarray(img, format="bgr24")
                 except Exception as e:
@@ -67,3 +69,4 @@ if task_name == task_list[0]:
 
     if ctx.video_processor:
         ctx.video_transformer.update_style(style_selection)
+        gc.collect()
